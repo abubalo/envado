a5a7d384f07cf871b1690a1aa789b8344dc32d6b
Abubakar Balogun <42701866+abubalo@users.noreply.github.com>
2023-11-16 20:18:56 +0200

    testing envgard function

--- example/example.ts
+++ example/example.ts
@@ -1,12 +1,16 @@
-import validateEnv from "../src/index";
+import envGuard from "../src/index";
 
-// Call the validateEnv function to validate and load environment variables
-const envConfig = validateEnv({
-  SECRET_KEY: "string",
+const envConfig = envGuard({
+  SECRET_KEY: { type: "string" },
+  API_KEY: { type: "string", defaultValue: "default-api-key" },
+  ENABLE_FEATURE: { type: "boolean" },
+  DEBUG_MODE: { type: "boolean", defaultValue: false },
+  CONFIG_JSON: { type: "object", defaultValue: {} },
+  TAGS: { type: "array", defaultValue: [] },
+  PORT: { type: "number", defaultValue: 5000 },
 });
 
-export { validateEnv };
-// console.log("Environment Configuration:", envConfig);
-
-// Access the validated values directly
 console.log("SECRET_KEY:", envConfig.SECRET_KEY);
+console.log("API_KEY:", envConfig.API_KEY);
+console.log("PORT:", envConfig.PORT);
+console.log("PORT datatype:", typeof envConfig.PORT);
