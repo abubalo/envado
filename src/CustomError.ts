87334690adb8475fd7edd330f8bad2fb1ac4e44c
Abubakar Balogun <42701866+abubalo@users.noreply.github.com>
2024-01-20 10:48:03 +0200

    refactor: Improve Error Messages in EnvGuard
    Refactored the error classes in the EnvGuard library to provide more informative and customizable error messages. This change improves the clarity of error messages and makes it easier for users to understand and troubleshoot issues related to missing or invalid environment variables.

--- src/CustomError.ts
+++ src/CustomError.ts
@@ -1,13 +1,13 @@
 export class MissingEnvVariableError extends Error {
-  constructor(envName: string) {
-    super(`No environment variable matches "${envName}" in .env`);
+  constructor(errorMessage: string) {
+    super(errorMessage);
     this.name = "MissingEnvVariableError";
   }
 }
 
 export class InvalidEnvVariableError extends Error {
-  constructor(envName: string, value: string) {
-    super(`Environment variable "${envName}" is invalid: ${value}`);
+  constructor(errorMessage: string) {
+    super(errorMessage);
     this.name = "InvalidEnvVariableError";
   }
 }
